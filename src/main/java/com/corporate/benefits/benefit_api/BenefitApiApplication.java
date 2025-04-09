c2ea73afe877f0413e260680fd1847719afd2950
Giovani e Jesuino <giovani.eugenio.sp@gmail.com>
2025-04-09 20:40:58 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/corporate/benefits/benefit_api/BenefitApiApplication.java
@@ -0,0 +1,13 @@
+package com.corporate.benefits.benefit_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenefitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenefitApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corporate/benefits/benefit_api/BenefitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corporate.benefits.benefit_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenefitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
