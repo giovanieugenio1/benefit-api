608f532b89124ab073b3e1f0736732e6f48fa35c
Giovani e Jesuino <giovani.eugenio.sp@gmail.com>
2025-04-09 22:41:36 -0300

    definindo exceptions e criando uma classe centralizadora de exceptions

---
+++ src/main/java/com/corporate/benefits/benefit_api/exceptions/handle/GlobalExceptionHandler.java
@@ -0,0 +1,57 @@
+package com.corporate.benefits.benefit_api.exceptions.handle;
+
+import com.corporate.benefits.benefit_api.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleResourceNotFound(ResourceNotFoundException ex) {
+        Map<String, Object> values = new HashMap<>();
+        values.put("timestamp", LocalDateTime.now());
+        values.put("status", HttpStatus.NOT_FOUND.value());
+        values.put("error", "Resource not found!");
+        values.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(values);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, Object> errors = new HashMap<>();
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                    errors.put(error.getField(), error.getDefaultMessage())
+                );
+
+        Map<String, Object> values = new HashMap<>();
+        values.put("timestamp", LocalDateTime.now());
+        values.put("status", HttpStatus.BAD_REQUEST.value());
+        values.put("error", "Validation error");
+        values.put("message", errors);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(values);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGenericException(Exception ex) {
+        Map<String, Object> values = new HashMap<>();
+
+        values.put("timestamp", LocalDateTime.now());
+        values.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        values.put("error", "Internal Server Error");
+        values.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(values);
+    }
+}
