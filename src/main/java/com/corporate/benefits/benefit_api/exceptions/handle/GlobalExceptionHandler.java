794e2133bc8ec48836e5a3b49e53ab09e7c357f3
Giovani e Jesuino <giovani.eugenio.sp@gmail.com>
2025-04-10 13:14:57 -0300

    corrigido método nas exceptions globais

--- src/main/java/com/corporate/benefits/benefit_api/exceptions/handle/GlobalExceptionHandler.java
+++ src/main/java/com/corporate/benefits/benefit_api/exceptions/handle/GlobalExceptionHandler.java
@@ -27,24 +27,6 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(values);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, Object> errors = new HashMap<>();
-        ex.getBindingResult()
-                .getFieldErrors()
-                .forEach(error ->
-                    errors.put(error.getField(), error.getDefaultMessage())
-                );
-
-        Map<String, Object> values = new HashMap<>();
-        values.put("timestamp", LocalDateTime.now());
-        values.put("status", HttpStatus.BAD_REQUEST.value());
-        values.put("error", "Validation error");
-        values.put("message", errors);
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(values);
-    }
-
     @ExceptionHandler(CpfValidationException.class)
     public ResponseEntity<Map<String, Object>> handleCpfValidationException(CpfValidationException ex) {
         Map<String, Object> values = new HashMap<>();
@@ -57,7 +39,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(EmailValidationException.class)
-    public ResponseEntity<Map<String, Object>> handleCpfValidationException(EmailValidationException ex) {
+    public ResponseEntity<Map<String, Object>> handleEmailValidationException(EmailValidationException ex) {
         Map<String, Object> values = new HashMap<>();
         values.put("timestamp", LocalDateTime.now());
         values.put("status", HttpStatus.BAD_REQUEST.value());
