0094aef88ea64dbb33b60c40b24e53a41e542a63
Giovani e Jesuino <giovani.eugenio.sp@gmail.com>
2025-04-10 15:33:12 -0300

    adionado a exception para lidar com argumentos inválidos gerais

--- src/main/java/com/corporate/benefits/benefit_api/exceptions/handle/GlobalExceptionHandler.java
+++ src/main/java/com/corporate/benefits/benefit_api/exceptions/handle/GlobalExceptionHandler.java
@@ -1,8 +1,6 @@
 package com.corporate.benefits.benefit_api.exceptions.handle;
 
-import com.corporate.benefits.benefit_api.exceptions.CpfValidationException;
-import com.corporate.benefits.benefit_api.exceptions.EmailValidationException;
-import com.corporate.benefits.benefit_api.exceptions.ResourceNotFoundException;
+import com.corporate.benefits.benefit_api.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -27,6 +25,23 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(values);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        Map<String, Object> values = new HashMap<>();
+        values.put("timestamp", LocalDateTime.now());
+        values.put("status", HttpStatus.BAD_REQUEST.value());
+        values.put("error", "Validation error");
+        values.put("message", errors);
+
+        return ResponseEntity.badRequest().body(values);
+    }
+
     @ExceptionHandler(CpfValidationException.class)
     public ResponseEntity<Map<String, Object>> handleCpfValidationException(CpfValidationException ex) {
         Map<String, Object> values = new HashMap<>();
@@ -40,6 +55,28 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmailValidationException.class)
     public ResponseEntity<Map<String, Object>> handleEmailValidationException(EmailValidationException ex) {
+        Map<String, Object> values = new HashMap<>();
+        values.put("timestamp", LocalDateTime.now());
+        values.put("status", HttpStatus.BAD_REQUEST.value());
+        values.put("error", "Validation error");
+        values.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(values);
+    }
+
+    @ExceptionHandler(BenefitAlreadyExistsException.class)
+    public ResponseEntity<Map<String, Object>> handleBenefitNameValidationException(BenefitAlreadyExistsException ex) {
+        Map<String, Object> values = new HashMap<>();
+        values.put("timestamp", LocalDateTime.now());
+        values.put("status", HttpStatus.BAD_REQUEST.value());
+        values.put("error", "Validation error");
+        values.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(values);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<Map<String, Object>> handleUserValidationException(UserAlreadyExistsException ex) {
         Map<String, Object> values = new HashMap<>();
         values.put("timestamp", LocalDateTime.now());
         values.put("status", HttpStatus.BAD_REQUEST.value());
