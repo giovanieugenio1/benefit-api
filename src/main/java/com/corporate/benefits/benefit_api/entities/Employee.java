f124066b62bbae3396ba2741e53c76f3ec411b17
Giovani e Jesuino <giovani.eugenio.sp@gmail.com>
2025-04-10 13:16:08 -0300

    adicionado o mapeamento do Swagger no application.properties

--- src/main/java/com/corporate/benefits/benefit_api/entities/Employee.java
+++ src/main/java/com/corporate/benefits/benefit_api/entities/Employee.java
@@ -27,7 +27,7 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false)
     private String name;
 
     @Column(nullable = false, unique = true)
