69a0a21d133c13621340b24faf80ec6061639085
Giovani e Jesuino <giovani.eugenio.sp@gmail.com>
2025-04-10 15:42:02 -0300

    criado os endpoints para o User

---
+++ src/main/java/com/corporate/benefits/benefit_api/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.corporate.benefits.benefit_api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+
+
+}
