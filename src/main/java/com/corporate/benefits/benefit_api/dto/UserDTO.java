d483138fbde4a93e8e87c6bd6302d9ab997cf15d
Giovani e Jesuino <giovani.eugenio.sp@gmail.com>
2025-04-10 15:40:26 -0300

    criado o DTO para User

---
+++ src/main/java/com/corporate/benefits/benefit_api/dto/UserDTO.java
@@ -0,0 +1,24 @@
+package com.corporate.benefits.benefit_api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Username must not be blank")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 6, message = "Password must have at least 6 characters")
+    private String password;
+
+    @NotNull(message = "Role must not be null")
+    private String role;
+}
