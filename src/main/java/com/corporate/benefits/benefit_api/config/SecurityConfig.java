a29313f3d1980392a05f6e4041a85c509d2b9648
Giovani e Jesuino <giovani.eugenio.sp@gmail.com>
2025-04-11 13:59:45 -0300

    adaptando minha classe de configuração para um novo contexto de segurança

--- src/main/java/com/corporate/benefits/benefit_api/config/SecurityConfig.java
+++ src/main/java/com/corporate/benefits/benefit_api/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**", "swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
+
                 )
                 .authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
