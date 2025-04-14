0ecc406b21adce3b953c597a1ddeeb1954e0e510
Giovani e Jesuino <giovani.eugenio.sp@gmail.com>
2025-04-14 16:52:12 -0300

    refatoração da classe AssignmentDTO

--- src/main/java/com/corporate/benefits/benefit_api/dto/AssignmentDTO.java
+++ src/main/java/com/corporate/benefits/benefit_api/dto/AssignmentDTO.java
@@ -1,29 +1,25 @@
 package com.corporate.benefits.benefit_api.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter @Setter
 public class AssignmentDTO {
-
     private Long id;
 
-    @JsonProperty("employeeId")
     @NotNull(message = "Employee ID must not be null")
     private Long employeeId;
 
-    @JsonProperty("benefitId")
     @NotNull(message = "Benefit ID must not be null")
     private Long benefitId;
 
-    @JsonProperty("assignedDate")
-    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "The date must be in the format yyyy-MM-dd")
+    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Date format must be yyyy-MM-dd")
     private String assignedDate;
+
+    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Date format must be yyyy-MM-dd")
+    private String endDate;
+
+    private Boolean active;
 }
